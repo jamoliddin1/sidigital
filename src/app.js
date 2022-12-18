bd3e983b44c45a586504c8ff07702c5288bfaa81
jamoliddin1 <Chkb007147@gmail.com>
2022-12-18 11:07:04 +0500

    darkness bg of formula box

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const nav = document.getElementById("main-nav");
 window.addEventListener("scroll", () => {
 	let scrollTopState = window.pageYOffset || document.documentElement.scrollTop;
 	if(scrollTopState > scrollTop) {
-		nav.style.top="-80px";
+		nav.style.top="-82px";
 	} else {
 		nav.style.top="0";
 	}
